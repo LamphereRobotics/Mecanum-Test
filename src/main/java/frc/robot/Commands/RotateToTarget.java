befe03d86ee611bac1b216e390ebc2d2b79297b6
nunu <lamphererobotics@lamphereschools.org>
2023-02-13 17:11:25 -0500

    Hopefully PID Loop

--- src/main/java/frc/robot/Commands/RotateToTarget.java
+++ src/main/java/frc/robot/Commands/RotateToTarget.java
@@ -4,12 +4,14 @@
 
 package frc.robot.Commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Sensors;
 public class RotateToTarget extends CommandBase {
   private final DriveSubsystem m_driveSub;
   private boolean done = false;
+  private double kP = 0.5;
   /** Creates a new RotateToTarget. */
   public RotateToTarget(DriveSubsystem driveSub) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -25,14 +27,15 @@ public class RotateToTarget extends CommandBase {
   @Override
   public void execute() {
     double xDist = Sensors.x.get();
-    if(xDist < 10 && xDist > -10){   
+    PIDController pid = new PIDController(kP, 0, 0);
+    if(xDist < 0.1 && xDist > -0.1){   
       m_driveSub.driveCommand(0, 0, 0, true);
       done = true;
         }else{
             if(xDist > 0){
-              m_driveSub.driveCommand(0, 0, -0.05, true);
+              m_driveSub.driveCommand(0, 0, pid.calculate(xDist, 0), true);
             }else{
-              m_driveSub.driveCommand(0, 0, 0.05, true);
+              m_driveSub.driveCommand(0, 0, pid.calculate(xDist, 0), true);
      }
     }
   }
